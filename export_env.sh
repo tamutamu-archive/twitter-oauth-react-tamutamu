d97efdbd0c9e6feb0a391801e394b395b7f987e4
tamutamu <tamu.0.0.tamu@gmail.com>
2021-03-05 15:09:35 +0900

    first commit

---
+++ export_env.sh
@@ -0,0 +1,6 @@
+ export FRONTEND_URL=http://localhost:3000
+ export HOST=localhost
+ export DANGEROUSLY_DISABLE_HOST_CHECK=true
+ export CONSUMER_KEY=eUIqyXqFXmnkXoshTAZV4VceN
+ export CONSUMER_SECRET=UUh9fVyAFmChDcFuuQTITuZeGWRv2iMtPxOyDm9Jh5Szft8BKL
+ export REACT_APP_SERVERLESS=http://localhost:3000
